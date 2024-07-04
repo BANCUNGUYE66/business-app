ee174bf0cba0e22b38f11a806f463094bcd10ce3
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2024-07-04 22:44:23 +0200

    final Authentication

--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,9 +1,43 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import * as WebBrowser from "expo-web-browser";
 import React from 'react'
 import { Colors } from '@/constants/Colors'
+import { Link } from "expo-router";
+import { useOAuth } from "@clerk/clerk-expo";
+import * as Linking from "expo-linking"
+
+export const useWarmUpBrowser = () => {
+    React.useEffect(() => {
+      // Warm up the android browser to improve UX
+      // https://docs.expo.dev/guides/authentication/#improving-user-experience
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
+    }, []);
+  };
 
 
 export default function LoginScreen() {
+
+    useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({ redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" })});
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
 
   return (
     <View>
@@ -39,7 +73,7 @@ export default function LoginScreen() {
                 marginVertical:15,
                 color:Colors.GRAY
             }}>Find your favorite business near you and post your own business to your community</Text>
-            <TouchableOpacity style={styles.btn}>
+            <TouchableOpacity style={styles.btn} onPress={onPress}>
                 <Text style={{
                     textAlign: 'center',
                     color:'#fff',
